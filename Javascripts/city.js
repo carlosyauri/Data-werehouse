0b9d1bb6e10b7821e2aaeafb7164a6fe23500a46
carlosyauri <carlosyauri97@gmail.com>
2021-05-28 14:43:53 -0300

    regiones paises y ciudades

--- Javascripts/city.js
+++ Javascripts/city.js
@@ -1,3 +1,22 @@
+
+let newRegion = document.getElementById("button-new-region")
+let fondoNegro = document.getElementById("fondoNegro");
+let containerContacto = document.getElementById("containerContacto");
+var cerrarNuevaRegion = document.getElementById("nvoContacto");
+
+newRegion.addEventListener("click", () => {
+  fondoNegro.classList.toggle("noDisplay")
+  containerContacto.classList.toggle("noDisplay")
+})
+
+
+cerrarNuevaRegion.addEventListener("click", () => {
+  fondoNegro.classList.toggle("noDisplay")
+  containerContacto.classList.toggle("noDisplay")
+})
+
+
+
 ////////////////////// FUNCION GET REGIONES //////////////////////
 
 let getLogin = async () => {
@@ -22,106 +41,129 @@ if(res.exito){
   }
 
 }
+
+
+
+
 async function completar (){
 
+    ///////////////////// OBTENEMOS ARRAY DE BASE DE DATOS CON GET ////////////////////////
+
     let arrayRegiones = await getLogin()
-    console.log(arrayRegiones.regiones[0])
-
-
 
     ////////////////////// COMPLETAR REGIONES ////////////////////////
 
     let nuevaRegion = document.getElementById("regionesNuevas");
-
     let ul = document.createElement("ul");
     ul.className = "myUL"
 
-    let li = document.createElement("li");
-    li.className = "region"
 
-    let div1 = document.createElement("div");
-    div1.classList="caret region-tittle"
-    let p = document.createElement("p");
-    p.innerHTML = `${arrayRegiones.regiones[0].nombre}`
-    div1.appendChild(p)
+    for (let i = 0; i < arrayRegiones.regiones.length; i++) {
 
-    let div2 = document.createElement("div");
-    div2.classList = "pais-ag noMostrar"
-    let button = document.createElement("button");
-    button.className = "pais"
-    button.innerHTML = "Agregar Pais"
-    div2.appendChild(button)
+      let li = document.createElement("li");
+      li.className = "region"
 
+      let div1 = document.createElement("div");
+      div1.classList="caret region-tittle"
+      let p = document.createElement("p");
+      p.innerHTML = arrayRegiones.regiones[i].nombre
+      div1.appendChild(p)
 
+      let div2 = document.createElement("div");
+      div2.classList = "pais-ag noMostrar"
+      let button = document.createElement("button");
+      button.className = "pais"
+      button.innerHTML = "Agregar Pais"
+      div2.appendChild(button)
 
-
-
-    let ul2 = document.createElement("ul")
-    ul2.className = "nested"
-
-    let li2 = document.createElement("li")
-    let div3 = document.createElement("div")
-    div3.classList = "caret country"
-    div3.innerHTML = `${arrayRegiones.regiones[0].Pais[0].nombre}`
-    li2.appendChild(div3)
-
-
-    let div4 = document.createElement("div")
-    div4.className = "container-btn"
-    let div5 = document.createElement("div")
-    let editar = document.createElement("button")
-    let eliminar = document.createElement("button")
-    editar.innerHTML = "Editar"
-    eliminar.innerHTML = "Eliminar"
-    div5.appendChild(editar)
-    div5.appendChild(eliminar)
-    let AgregarCiudad = document.createElement("button")
-    AgregarCiudad.className = "ciudad"
-    AgregarCiudad.innerHTML = "Agregar Ciudad"
-    div4.appendChild(div5)
-    div4.appendChild(AgregarCiudad)
-    li2.appendChild(div4) 
+      li.appendChild(div1)
+      li.appendChild(div2)
 
 
 
-    let ulCitys = document.createElement("ul");
-    ulCitys.classList = "nested citys"
-    let divPcia = document.createElement("div")
-    let liPcia = document.createElement("li")
-    liPcia.className = "cia"
-    liPcia.innerHTML = `${arrayRegiones.regiones[0].Pais[0].Ciudads[0].nombre}`
-    let span = document.createElement("span")
-    let i1 = document.createElement("i")
-    i1.classList = "far fa-edit"
-    let i2= document.createElement("i")
-    i2.classList = "far fa-trash-alt"
+      ////////////////////// COMPLETAR PAISES ////////////////////////
+
+      let ul2 = document.createElement("ul")
+      ul2.className = "nested"
+
+      for (let j = 0; j < arrayRegiones.regiones[i].Pais.length; j++) {
 
 
-    span.appendChild(i1)
-    span.appendChild(i2)
-    liPcia.appendChild(span)
-    divPcia.appendChild(liPcia)
-    ulCitys.appendChild(divPcia)
-    li2.appendChild(ulCitys)
-    ul2.appendChild(li2)
+          let li2 = document.createElement("li")
+          let div3 = document.createElement("div")
+          div3.classList = "caret country"
+          div3.innerHTML = arrayRegiones.regiones[i].Pais[j].nombre
+          li2.appendChild(div3)
 
-    li.appendChild(div1)
-    li.appendChild(div2)
-    li.appendChild(ul2)
-    ul.appendChild(li)
+
+          let div4 = document.createElement("div")
+          div4.className = "container-btn"
+          let div5 = document.createElement("div")
+          let editar = document.createElement("button")
+          let eliminar = document.createElement("button")
+          editar.innerHTML = "Editar"
+          eliminar.innerHTML = "Eliminar"
+          div5.appendChild(editar)
+          div5.appendChild(eliminar)
+          let AgregarCiudad = document.createElement("button")
+          AgregarCiudad.className = "ciudad"
+          AgregarCiudad.innerHTML = "Agregar Ciudad"
+          div4.appendChild(div5)
+          div4.appendChild(AgregarCiudad)
+          li2.appendChild(div4)    
+
+          ul2.appendChild(li2)
+
+
+
+          ////////////////////// COMPLETAR CIUDADES ////////////////////////
+
+          let ulCitys = document.createElement("ul");
+          ulCitys.classList = "nested citys"
+          
+          for (let k = 0; k < arrayRegiones.regiones[i].Pais[j].Ciudads.length; k++) {
+            
+      
+            let divPcia = document.createElement("div")
+            let liPcia = document.createElement("li")
+            liPcia.className = "cia"
+            liPcia.innerHTML = arrayRegiones.regiones[i].Pais[j].Ciudads[k].nombre
+            let span = document.createElement("span")
+            let i1 = document.createElement("i")
+            i1.classList = "far fa-edit"
+            let i2= document.createElement("i")
+            i2.classList = "far fa-trash-alt"
+            span.appendChild(i1)
+            span.appendChild(i2)
+
+
+            liPcia.appendChild(span)
+            divPcia.appendChild(liPcia)
+            ulCitys.appendChild(divPcia)
+
+          }
+          
+          
+          li2.appendChild(ulCitys)
+
+
+      }
+
+      li.appendChild(ul2)
+      ul.appendChild(li)
+
+    }
+    
     nuevaRegion.appendChild(ul)
 
 
 
 
+    ////////////////////// EVENTOS CRUD ////////////////////////
 
     var toggler = document.getElementsByClassName("caret");
     var i;
-    var nvoRegion = document.getElementById("button-new-region");
     var fondoNegro = document.getElementById("fondoNegro");
-    var containerContacto = document.getElementById("containerContacto");
-    var cerrarNuevaRegion = document.getElementById("nvoContacto");
-    var newRegion = document.getElementById("button-new-region")
     var pais = document.getElementsByClassName("pais");
     var ciudad = document.getElementsByClassName("ciudad");
     var containerPais = document.getElementById("containerPais");
@@ -139,22 +181,6 @@ async function completar (){
         
       });
     }
-
-
-
-    nvoRegion.addEventListener("click", () => {
-        fondoNegro.classList.toggle("noDisplay")
-        containerContacto.classList.toggle("noDisplay")
-    })
-
-
-    cerrarNuevaRegion.addEventListener("click", () => {
-        fondoNegro.classList.toggle("noDisplay")
-        containerContacto.classList.toggle("noDisplay")
-    })
-
-
-
 
     for (i = 0; i < pais.length; i++){
 
@@ -190,101 +216,11 @@ async function completar (){
       
     })
 
-
-
-
-
-
-
 }
 
 
+
 completar()
-
-
-////////////////////// COMPLETAR REGIONES ////////////////////////
-
-// let nuevaRegion = document.getElementById("regionesNuevas");
-
-// let ul = document.createElement("ul");
-// ul.className = "myUL"
-
-// let li = document.createElement("li");
-// li.className = "region"
-
-// let div1 = document.createElement("div");
-// div1.classList="caret region-tittle"
-// let p = document.createElement("p");
-// p.innerHTML = "Region creada"
-// div1.appendChild(p)
-
-// let div2 = document.createElement("div");
-// div2.classList = "pais-ag noMostrar"
-// let button = document.createElement("button");
-// button.className = "pais"
-// button.innerHTML = "Agregar PAIS"
-// div2.appendChild(button)
-
-
-
-
-
-// let ul2 = document.createElement("ul")
-// ul2.className = "nested"
-
-// let li2 = document.createElement("li")
-// let div3 = document.createElement("div")
-// div3.classList = "caret country"
-// div3.innerHTML = "PAIS CREADO"
-// li2.appendChild(div3)
-
-
-// let div4 = document.createElement("div")
-// div4.className = "container-btn"
-// let div5 = document.createElement("div")
-// let editar = document.createElement("button")
-// let eliminar = document.createElement("button")
-// editar.innerHTML = "Editar"
-// eliminar.innerHTML = "Eliminar"
-// div5.appendChild(editar)
-// div5.appendChild(eliminar)
-// let AgregarCiudad = document.createElement("button")
-// AgregarCiudad.className = "ciudad"
-// AgregarCiudad.innerHTML = "Agregar CIUDAD"
-// div4.appendChild(div5)
-// div4.appendChild(AgregarCiudad)
-// li2.appendChild(div4) 
-
-
-
-// let ulCitys = document.createElement("ul");
-// ulCitys.classList = "nested citys"
-// let divPcia = document.createElement("div")
-// let liPcia = document.createElement("li")
-// liPcia.className = "cia"
-// liPcia.innerHTML = "PROVINCIA CREADA"
-// let span = document.createElement("span")
-// let i1 = document.createElement("i")
-// i1.classList = "far fa-edit"
-// let i2= document.createElement("i")
-// i2.classList = "far fa-trash-alt"
-
-
-// span.appendChild(i1)
-// span.appendChild(i2)
-// liPcia.appendChild(span)
-// divPcia.appendChild(liPcia)
-// ulCitys.appendChild(divPcia)
-// li2.appendChild(ulCitys)
-// ul2.appendChild(li2)
-
-// li.appendChild(div1)
-// li.appendChild(div2)
-// li.appendChild(ul2)
-// ul.appendChild(li)
-// nuevaRegion.appendChild(ul)
-
-
 
 
 
