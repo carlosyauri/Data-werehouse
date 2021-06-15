a83656123321dfa6f067efa0b0d2a86258548fb1
carlosyauri <carlosyauri97@gmail.com>
2021-06-15 11:23:25 -0300

    companias

--- Javascripts/compania.js
+++ Javascripts/compania.js
@@ -13,9 +13,24 @@ let getCompania = async () => {
 
 }
 
+let getCiudades = async () => {
+
+    let searchApi = await fetch (`http://localhost:3000/regiones/ciudades`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    return await searchApi.json()
+    
+
+}
+
 
 async function completarCompanias (){
 
+    
     let companias = await getCompania()
     let array = []
 
@@ -49,20 +64,80 @@ async function completarCompanias (){
 
 }
 
-let fondoNegro = document.getElementById("fondoNegro")
-let container = document.getElementById("containerContacto")
+completarCompanias()
+
 
 let btnAgregarCompania = document.getElementById("btn-compania")
+let fondoNegro = document.getElementById("fondoNegro")
+let container = document.getElementById("containerContacto")
+let cerrarAgregar = document.getElementById("nvoContacto")
+let guardar = document.getElementById("guardar")
+let cancelar = document.getElementById("cancelar")
+
+cerrarAgregar.addEventListener("click", () => {
+    fondoNegro.classList.toggle("noDisplay")
+    container.classList.toggle("noDisplay")
+})
+
+cancelar.addEventListener("click", () => {
+    fondoNegro.classList.toggle("noDisplay")
+    container.classList.toggle("noDisplay")
+})
+
+
 btnAgregarCompania.addEventListener("click", () => {
 
     fondoNegro.classList.toggle("noDisplay")
     container.classList.toggle("noDisplay")
-
-    // postCompania("Yauri SA", "Av. asd 123", "yauri@gmail.com", "4947658", "2")
-    // location.href = "../html/compania.html"
 })
 
-completarCompanias()
+let nombre = document.getElementById("nombre")
+let direccion = document.getElementById("direccion")
+let email = document.getElementById("email")
+let telefono = document.getElementById("telefono")
+let idCiudad;
+
+guardar.addEventListener("click", () => {
+
+    postCompania(nombre.value, direccion.value, email.value, telefono.value, idCiudad)
+    location.href = "../html/compania.html" 
+
+})
+
+async function agregarCiudades () {
+
+
+    let selectCiudad = document.getElementById("ciudad")
+    let ciudades = await getCiudades();
+
+    for (let i = 0; i < ciudades.ciudades.length; i++) {
+
+        let option = document.createElement("option")
+        option.innerHTML = ciudades.ciudades[i].nombre
+
+        selectCiudad.appendChild(option) 
+
+
+        selectCiudad.addEventListener("click", async(e) => {
+
+            let ciudadIngresada = e.target.value
+    
+            if(ciudadIngresada == ciudades.ciudades[i].nombre){
+                idCiudad = ciudades.ciudades[i].id
+            }
+
+        })
+
+    }
+
+
+      
+}
+
+agregarCiudades()
+
+
+
 
 
 
